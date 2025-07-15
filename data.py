642b91e5152c59df0b9f0e762212085fe0ca014b
AsteroidCap4040 <prithvidevadiga48@gmail.com>
2025-07-15 15:37:21 +0530

    Removed API Request

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 import requests
 
 #Getting the response from the api
-response = requests.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_net8rRd48e8Q435gpRmK2ouGvencWF1Ci0IeQg7C") #Place your API from 'Currency API'
+response = requests.get("") #Place your API from 'Currency API'
 response.raise_for_status() ##Raising an error if there is status problem
 value = response.json() #converting the data into JSON datatype
 
